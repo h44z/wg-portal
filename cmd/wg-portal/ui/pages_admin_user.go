c2ca569d9160a4caa5c30b7a982eb1b413be1b58
Christoph Haas <christoph.h@sprinternet.at>
2022-01-16 22:55:35 +0100

    user index page

--- cmd/wg-portal/ui/pages_admin_user.go
+++ cmd/wg-portal/ui/pages_admin_user.go
@@ -9,14 +9,17 @@ import (
 func (h *handler) handleAdminUserIndexGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		currentSession := h.session.GetData(c)
+		users, err := h.backend.GetAllUsers()
+		if err != nil {
+			// TODO
+		}
 
 		c.HTML(http.StatusOK, "admin_user_index.gohtml", gin.H{
-			"Route":          c.Request.URL.Path,
-			"Alerts":         h.session.GetFlashes(c),
-			"Session":        currentSession,
-			"Static":         h.getStaticData(),
-			"Interface":      nil, // TODO: load interface specified in the session
-			"InterfaceNames": map[string]string{"wgX": "wgX descr"},
+			"Route":   c.Request.URL.Path,
+			"Alerts":  h.session.GetFlashes(c),
+			"Session": currentSession,
+			"Static":  h.getStaticData(),
+			"Users":   users,
 		})
 	}
 }
