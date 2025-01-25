d35889de73b8c40b5138ced8082b2db136c79df7
Christoph Haas <christoph.h@sprinternet.at>
2025-01-25 23:06:44 +0100

    remove external google fonts (#107)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,13 +9,14 @@ import i18n from "./lang";
 import Notifications from '@kyvg/vue3-notification'
 
 // Bootstrap (and theme)
-//import "bootstrap/dist/css/bootstrap.min.css"
-import "bootswatch/dist/lux/bootstrap.min.css";
+import "@/assets/custom.scss";
 import "bootstrap";
 import "./assets/base.css";
 
-// Fontawesome
+// Fonts
 import "@fortawesome/fontawesome-free/js/all.js"
+import "@fontsource/nunito-sans/400.css";
+import "@fontsource/nunito-sans/600.css";
 
 // Flags
 import "flag-icons/css/flag-icons.min.css"
