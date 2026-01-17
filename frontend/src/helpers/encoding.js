6e47d8c3e91a83bfdae10f97b740a22d247fb847
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-01-17 19:38:48 +0100

    fix: parity of Base64/URL encoding between frontend and backend (#611)
    Signed-off-by: Arnaud Rocher <arnaud.roche3@gmail.com>
    (cherry picked from commit 5d58df8a193f6f455ebf9e1c76237a34c42139d3)

--- frontend/src/helpers/encoding.js
+++ frontend/src/helpers/encoding.js
@@ -1,7 +1,7 @@
 export function base64_url_encode(input) {
   let output = btoa(input)
-  output = output.replace('+', '.')
-  output = output.replace('/', '_')
-  output = output.replace('=', '-')
+  output = output.replaceAll('+', '.')
+  output = output.replaceAll('/', '_')
+  output = output.replaceAll('=', '-')
   return output
 }
